dbf8efe559e773c35e631a30a0b4fd728c68066b
Aitor Jurquet <aitor.jurquet@gmail.com>
2022-05-19 10:18:08 +0200

    Routes and scroll down achieve

--- src/education/Education.jsx
+++ src/education/Education.jsx
@@ -1,4 +1,8 @@
 const Education = () => {
-  return <div>Education</div>;
+  return (
+    <div className="_education">
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 export default Education;
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,17 +1,59 @@
-const NavBar = () => {
+import PropTypes from "prop-types";
+import handleScroll from "../utils/scroll";
+
+const NavBar = ({ homeRef, skillsRef, educationRef, projectsRef }) => {
   return (
     <div className="_navbar">
       <h2 className="_navbar__name">Aitor_Jurquet</h2>
       <nav className="_navbar__container">
         <ul className="_navbar__container__ul">
-          <li className="_navbar__container__ul__li">Home</li>
-          <li className="_navbar__container__ul__li">Skills</li>
-          <li className="_navbar__container__ul__li">Education</li>
-          <li className="_navbar__container__ul__li">Projects</li>
+          <li
+            className="_navbar__container__ul__li"
+            onClick={() => handleScroll(homeRef)}
+            area-hidden="true"
+            onKeyDown={() => handleScroll(homeRef)}
+            role="presentation"
+          >
+            Home
+          </li>
+          <li
+            className="_navbar__container__ul__li"
+            onClick={() => handleScroll(skillsRef)}
+            area-hidden="true"
+            onKeyDown={() => handleScroll(skillsRef)}
+            role="presentation"
+          >
+            Skills
+          </li>
+          <li
+            className="_navbar__container__ul__li"
+            onClick={() => handleScroll(educationRef)}
+            area-hidden="true"
+            onKeyDown={() => handleScroll(educationRef)}
+            role="presentation"
+          >
+            Education
+          </li>
+          <li
+            className="_navbar__container__ul__li"
+            onClick={() => handleScroll(projectsRef)}
+            area-hidden="true"
+            onKeyDown={() => handleScroll(projectsRef)}
+            role="presentation"
+          >
+            Projects
+          </li>
         </ul>
       </nav>
     </div>
   );
+};
+
+NavBar.propTypes = {
+  homeRef: PropTypes.func.isRequired,
+  skillsRef: PropTypes.func.isRequired,
+  educationRef: PropTypes.func.isRequired,
+  projectsRef: PropTypes.func.isRequired,
 };
 
 export default NavBar;
